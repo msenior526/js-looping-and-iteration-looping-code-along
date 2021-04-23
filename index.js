aa81d3ddcce13aba06f9c47b8b986025bf31948b
msenior526 <msenior526@gmail.com>
2021-04-23 19:16:43 -0400

    commit tree 30f403d1f57620ef93c2985b0144d88488301267

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventType) {
+    let thankYouMessage = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${eventType} gift!`);
+    }
+    return thankYouMessage;
+}
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer -= 1;
+    }
+    console.log(integer)
+}
